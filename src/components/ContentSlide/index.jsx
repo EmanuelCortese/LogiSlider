64f6e53707255ca7d868aeee51de4674954122ed
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2023-12-01 22:37:22 -0300

    feat: use Banner component and rename map parameters

--- src/components/ContentSlide/index.jsx
+++ src/components/ContentSlide/index.jsx
@@ -1,14 +1,29 @@
 import { fullSize } from '../../utils/constants'
 import { isVideo } from '../../utils/isVideo'
+import { Banner } from '../Banner/Banner'
 import Image from '../Image'
-import { mediaList } from '../mediaList'
 import Video from '../Video'
+import { mediaList } from '../mediaList'
 
 const ContentSlide = ({ refSlide }) => (
 	<div ref={refSlide} className={`flex ${fullSize}`}>
-		{mediaList.map((src) =>
-			isVideo(src) ? <Video key={src} src={src} /> : <Image key={src} src={src} />
-		)}
+		{mediaList.map((props) => {
+			return (
+				<div key={props.id} className={`flex ${fullSize} relative`}>
+					<Banner props={props} />
+
+					{isVideo(props.media) ? (
+						<>
+							<Video key={props.media} src={props.media} />
+						</>
+					) : (
+						<>
+							<Image key={props.media} src={props.media} />
+						</>
+					)}
+				</div>
+			)
+		})}
 	</div>
 )
 
