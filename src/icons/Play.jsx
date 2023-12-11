b73420b8578774bd79618c405767d35f1c8188c9
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2023-12-11 16:37:23 -0300

    feat: create Play and Pause button icons

---
+++ src/icons/Play.jsx
@@ -0,0 +1,5 @@
+export const Play = () => (
+	<svg role='img' height='24' width='24' aria-hidden='true' viewBox='0 0 24 24'>
+		<path fill='rgba(39, 39, 42, .8)' d='M8 5.14v14l11-7-11-7z'></path>
+	</svg>
+)
