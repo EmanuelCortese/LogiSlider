8690cf0e6c1bda461dfec161224a155ebf1620fd
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2023-12-11 16:52:16 -0300

    feat: add Play and Pause buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import { Button } from './components/Button'
 import ContentSlide from './components/ContentSlide'
 import { Header } from './components/Header'
 import { useSlide } from './hooks/useSlide'
+import { Pause } from './icons/Pause'
+import { Play } from './icons/Play'
 
 function App() {
-	const { slider, previous, next } = useSlide()
+	const { isPlaying, slider, previous, next, handleButton } = useSlide()
 
 	return (
 		<div className='h-screen w-screen flex items-center relative overflow-hidden'>
@@ -29,6 +31,12 @@ function App() {
 				location2='translate(266,0), rotate(180)'
 				nextPage={next}
 			/>
+			<button
+				onClick={handleButton}
+				className='flex items-center justify-center absolute bottom-6 right-6 h-10 w-10 z-10 bg-zinc-400/80 border-2 rounded-full border-zinc-800/80 hover:bg-zinc-400 transition-colors duration-500'
+			>
+				{isPlaying ? <Pause /> : <Play />}
+			</button>
 		</div>
 	)
 }
