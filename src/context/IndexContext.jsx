77334ac7aee5dbaf001a2e3fa788f8184137c58b
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2024-01-02 17:23:44 -0300

    feat: add modal state to global context

--- src/context/IndexContext.jsx
+++ src/context/IndexContext.jsx
@@ -4,8 +4,11 @@ const Context = createContext({})
 
 export const IndexProvider = ({ children }) => {
 	const [index, setIndex] = useState(0)
+	const [modal, setModal] = useState(null)
 
-	return <Context.Provider value={{ index, setIndex }}>{children}</Context.Provider>
+	return (
+		<Context.Provider value={{ index, setIndex, modal, setModal }}>{children}</Context.Provider>
+	)
 }
 
 export default Context
