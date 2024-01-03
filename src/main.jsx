e3a9bc1b487c44964c4fe1042324635941055daf
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2024-01-03 18:07:51 -0300

    refactor: change Provider name

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { IndexProvider } from './context/IndexContext'
+import { SliderProvider } from './context/SliderContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<IndexProvider>
+		<SliderProvider>
 			<App />
-		</IndexProvider>
+		</SliderProvider>
 	</React.StrictMode>
 )
